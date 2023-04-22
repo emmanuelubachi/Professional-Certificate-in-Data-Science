a5e341232a740a6957c81df3e4334bd2fd437255
Emmanuel Ubachi <37215825+emmanuelubachi@users.noreply.github.com>
2023-04-22 08:26:38 +0400

    Day 2 - Added Functions script

---
+++ 01 - R Basics/Section 1 - R Basics, Functions and Data Types/1.2 R Basics/Functions.R
@@ -0,0 +1,13 @@
+## Code to compute solution to quadratic equation of the form ax^2 + bx + c
+
+## define the variables
+a <- 3
+b <- 2
+c <- -1
+
+## now compute the solution
+solution_1 <- (-b + sqrt(b^2 - 4*a*c))/(2*a)
+solution_2 <- (-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+solution_1
+solution_2
