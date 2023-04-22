84c88062950246342757c9cdf368a5b5b52d1319
Emmanuel Ubachi <37215825+emmanuelubachi@users.noreply.github.com>
2023-04-22 08:30:12 +0400

    Day 2 - Added Assesment script

---
+++ 01 - R Basics/Section 1 - R Basics, Functions and Data Types/1.3 Data Types/Assessment.R
@@ -0,0 +1,21 @@
+# Solving the equation 2x^2 - x - 4 = 0
+
+# assigning values to variables
+a <- 2
+b <- -1
+c <- -4
+
+# solving the quadratic equation
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
+
+
+# log base 4 of 1024
+log(1024, 4)
+
+library(dslabs)
+data(movielens)
+str(movielens)
+
+nlevels(movielens$genres)
+levels(movielens$genres)
