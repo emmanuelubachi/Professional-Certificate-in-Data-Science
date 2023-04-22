5af3eba3b3a8e08b09647635c978b4d2bb4106a9
Emmanuel Ubachi <37215825+emmanuelubachi@users.noreply.github.com>
2023-04-22 08:27:05 +0400

    Day 2 - Added R Basics script

---
+++ 01 - R Basics/Section 1 - R Basics, Functions and Data Types/1.2 R Basics/R Basics.R
@@ -0,0 +1,12 @@
+# Solving the equation x^2 + x - 1 = 0
+
+# assigning values to variables
+a <- 1
+b <- 1
+c <- -1
+
+#ls()
+
+# solving the quadratic equation
+(-b + sqrt(b^2 - 4*a*c))/(2*a)
+(-b - sqrt(b^2 - 4*a*c))/(2*a)
